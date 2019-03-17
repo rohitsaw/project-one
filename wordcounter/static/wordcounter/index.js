a14dd78fa5720ac6b9eb206302570ce548dd5454
rohitsaw <rsaw409@gmail.com>
2019-03-18 02:48:25 +0530

    add wordcounter app

---
+++ wordcounter/static/wordcounter/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $(".btn").click(function(){
+    const text = $("textarea").val()
+    if (text.length > 0){
+      // alert("ready to ajax")
+      $.ajax(
+        {
+          url: "count",
+          data: {
+            text: text,
+          },
+          dataType: 'json',
+          success : function(data){
+              // alert(`success ${data}`)
+              console.log(data.most_frequent_word)
+              $(".total_word").text(data.word_count)
+              $(".total_character").text(data.letter_count)
+              $(".most_frequent").text(data.most_frequent_word)
+          }
+        })
+    }
+    else{
+      $(".total_word").text(0)
+      $(".total_character").text(0)
+      $(".most_frequent").text("None")
+    }
+  })
+});
---
+++ wordcounter/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+import operator
+
+# Create your views here.
+def index(request):
+    return render(request, "wordcounter/index.html")
+
+def count(request):
+    temp = request.GET.get("text", None)
+    lcount = len(temp)
+    temp = list(temp.strip().split())
+    d = {}
+    wcount = 0
+    for item in temp:
+        if item == "": continue
+        wcount += 1
+        d[item] = d.get(item,0) + 1
+    temp = 0
+    mfw = []
+    for key, value in d.items():
+        if value > temp:
+            temp = value
+            mfw = []
+            mfw.append(key)
+            if len(mfw) == 5:
+                break
+        elif value == temp:
+            mfw.append(key)
+            if len(mfw) == 5:
+                break
+    if len(mfw) == 5:
+        mfw[4] += " etc.."
+
+    #print(d)
+    #mfw = max(d.items(), key=operator.itemgetter(1))[0]
+    d = {"word_count": wcount, "letter_count":lcount, "most_frequent_word":mfw}
+    return JsonResponse(d)
