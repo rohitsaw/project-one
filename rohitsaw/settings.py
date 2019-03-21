eda7ede8b4084be3792d8430264fda6ae236f18a
rohitsaw <rsaw409@gmail.com>
2019-03-21 18:47:45 +0530

    add readme

--- rohitsaw/settings.py
+++ rohitsaw/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/myapp/photo')
-STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'userprofile/media')
+# STATIC_URL = '/static/'
+#
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'userprofile/media')
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'userprofile/media'),
