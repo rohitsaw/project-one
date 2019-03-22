eaa31fe45f5eece78ee17ffaa335453014621994
rohitsaw <rsaw409@gmail.com>
2019-03-23 04:06:23 +0530

    first try to deploy

--- rohitsaw/settings.py
+++ rohitsaw/settings.py
@@ -134,3 +134,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
