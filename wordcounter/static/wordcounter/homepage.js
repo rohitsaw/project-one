68a2cbb952f7f4d814404bf4667566f91568a585
rohitsaw <rsaw409@gmail.com>
2019-03-23 04:31:39 +0530

    minor

--- wordcounter/static/wordcounter/homepage.js
+++ wordcounter/static/wordcounter/homepage.js
@@ -4,6 +4,7 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {
+  swal("Welcome to MyApp", "click left-top option menu for details!", "info");
    var elems = document.querySelectorAll('.sidenav');
    var instances = M.Sidenav.init(elems);
  });
