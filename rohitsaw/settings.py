5516f95ccfeb1cbd340edbd773122a2de21734ca
rohitsaw <rsaw409@gmail.com>
2019-04-05 23:15:06 +0530

    minor

--- rohitsaw/settings.py
+++ rohitsaw/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(%txdpp+ruvfd8+y7o)qe(8q550^l_m&*ens#izm+(^^7)qjkz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
